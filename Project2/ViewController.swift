3dd882cde3d741892a05378afc62aafee20c4118
rezakoushki95 <rezaaarhus@hotmail.com>
2023-01-30 17:28:38 +0100

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Reza Koushki on 30/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
