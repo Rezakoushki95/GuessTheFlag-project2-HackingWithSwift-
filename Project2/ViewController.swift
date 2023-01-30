3ab432e150ce02375884d11a34a0a9489c2cbb38
rezakoushki95 <rezaaarhus@hotmail.com>
2023-01-30 18:34:00 +0100

    - Randomize the appearing flags - Use the correctAnswer value to set the title of nav controller (indicating which flag the user should guess)

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
 	
 	var countries = [String]()
 	var score = 0
+	var correctAnswer = 0
 	
 	
 	override func viewDidLoad() {
@@ -34,6 +35,11 @@ class ViewController: UIViewController {
 	}
 	
 	func askQuestion() {
+		countries.shuffle()
+		correctAnswer = Int.random(in: 0...2)
+		
+		title = "Where is: \(countries[correctAnswer].uppercased())"
+		
 		button1.setImage(UIImage(named: countries[0]), for: .normal)
 		button2.setImage(UIImage(named: countries[1]), for: .normal)
 		button3.setImage(UIImage(named: countries[2]), for: .normal)
